947850ab2edf612d3ea74a82b25faa100390787d
jackoyugi <oyugijack@gmail.com>
2020-08-02 08:18:28 +0300

    added main layout and styles colors

--- app/src/main/java/co/ke/biofit/haemotyping/activity/MainActivity.java
+++ app/src/main/java/co/ke/biofit/haemotyping/activity/MainActivity.java
@@ -1,27 +1,38 @@
-package co.ke.biofit.haemotyping;
+package co.ke.biofit.haemotyping.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
-import android.app.ActionBar;
+import android.annotation.SuppressLint;
+import androidx.appcompat.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.util.Log;
 
 import java.util.ArrayList;
 
+import co.ke.biofit.haemotyping.R;
+import co.ke.biofit.haemotyping.adapter.SectionsPagerAdapter;
+import co.ke.biofit.haemotyping.util.TabUtils;
+
+
 @SuppressWarnings("ALL")
-public class MainActivity extends AppCompatActivity {
-    private static final Fragment[] mFragments = {
-            new HomeFragment(),
+public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
+    private static final Fragment[] mFragments = new Fragment[]{
+            new Fragment(),
+            new Fragment()
 
     };
-    private ArrayList<Tab> mTabs;
-
+    private ArrayList<ActionBar.Tab> mTabs;
     private ViewPager mViewPager;
     private androidx.appcompat.app.ActionBar mActionBar;
     private SectionsPagerAdapter mSectionsPagerAdapter;
-
 
 
     @Override
@@ -31,28 +42,61 @@ public class MainActivity extends AppCompatActivity {
 
         setupActionBar();
     }
-    @Override
-    protected void onResume() {
-        super.onResume();
-        TabUtils.setUpdateCurrentTab(true);
-        mActionBar.selectTab(mTabs.get(TabUtils.getCurrentTabPosition()));
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        TabUtils.setUpdateCurrentTab(false);
-    }
     private void setupActionBar() {
         mActionBar = getSupportActionBar();
         mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
         setupPageViewer();
-
-        mTabs = new ArrayList<Tab>();
+        mTabs = new ArrayList<ActionBar.Tab>();
         for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            Tab t = mActionBar.newTab().setText(TabUtils.getTabTitleId(i)).setTabListener(this);
+            ActionBar.Tab t = mActionBar.newTab().setText(TabUtils.getTabTitleId(i)).setTabListener(this);
             mTabs.add(t);
             mActionBar.addTab(t);
         }
+    }
+
+    @SuppressLint("WrongViewCast")
+    private void setupPageViewer() {
+        mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager(), mFragments);
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
+            @Override
+            public void onPageSelected(int position) {
+                getSupportActionBar().setSelectedNavigationItem(position);
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        final MenuItem actionSnapIt = menu.findItem(R.id.action_snap_it);
+        MenuItemCompat.getActionView(actionSnapIt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onOptionsItemSelected(actionSnapIt);
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
+        TabUtils.setCurrentTabPosition(tab.getPosition());
+        mViewPager.setCurrentItem(tab.getPosition());
+
+    }
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
+    }
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
+
     }
 }
--- app/src/main/java/co/ke/biofit/haemotyping/util/TabUtils.java
+++ app/src/main/java/co/ke/biofit/haemotyping/util/TabUtils.java
@@ -1,4 +1,31 @@
 package co.ke.biofit.haemotyping.util;
 
+import co.ke.biofit.haemotyping.R;
+
 public class TabUtils {
+    public static final int TAB_COUNT = 4;
+
+    private static int currentTabPosition = 0;
+
+    private static boolean updateCurrentTab = true;
+
+    private static final int[] tabsTitlesIds = {
+            R.string.title_home_tab;
+
+
+    public static void setCurrentTabPosition(int position) {
+        if (updateCurrentTab) currentTabPosition = position;
+    }
+
+    public static int getTabTitleId(int position) {
+        return tabsTitlesIds[position];
+
+    }
+    public static int getCurrentTabPosition() {
+        return currentTabPosition;
+    }
+    public static void setUpdateCurrentTab(boolean update) {
+        updateCurrentTab = update;
+    }
+
 }
