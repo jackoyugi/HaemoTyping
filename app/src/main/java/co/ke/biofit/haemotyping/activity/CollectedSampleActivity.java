1d72c06797d61e0f79824eea3b11be1bc47f5b7a
jackoyugi <oyugijack@gmail.com>
2020-08-03 00:18:44 +0300

    added new layouts

---
+++ app/src/main/java/co/ke/biofit/haemotyping/activity/CollectedSampleActivity.java
@@ -0,0 +1,52 @@
+package co.ke.biofit.haemotyping.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import co.ke.biofit.haemotyping.R;
+import co.ke.biofit.haemotyping.adapter.HaemotypeArrayAdapter;
+
+public class CollectedSampleActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView) TextView mLocationTextView;
+    @BindView(R.id.listView) ListView mListView;
+
+    private String[] collectedSample = new String[] {"Jack Winter", "Winnie's Pinches",
+            "Seeds of Gold", "The Pupper", "Luc Lac", "Sweet Basil", "Grace Butt"};
+
+    private String[] bloodGroup = new String[] {"O+/Rh+", "A-/Rh-", "B+/Rh+", "A+/Rh-", "AB-/Rh-", "AB+/Rh+", "O+/Rh+" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collectedsample);
+        ButterKnife.bind(this);
+
+        HaemotypeArrayAdapter adapter = new HaemotypeArrayAdapter(this, android.R.layout.simple_list_item_1, collectedSample, bloodGroup);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView) view).getText().toString();
+                Log.v("RestraurantsActivity", "In the onItemClickListener!");
+                Toast.makeText(CollectedSampleActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        mLocationTextView.setText("Here are all the people with blood group() near you: " + location);
+        Log.d("CollectedSampleActivity", "In the onCreate method!");
+
+    }
+
+
+}
--- app/src/main/java/co/ke/biofit/haemotyping/activity/MainActivity.java
+++ app/src/main/java/co/ke/biofit/haemotyping/activity/MainActivity.java
@@ -1,125 +1,37 @@
 package co.ke.biofit.haemotyping.activity;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.MenuItemCompat;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.viewpager.widget.ViewPager;
-import android.annotation.SuppressLint;
-import androidx.appcompat.app.ActionBar;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.ArrayList;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import co.ke.biofit.haemotyping.R;
-import co.ke.biofit.haemotyping.adapter.SectionsPagerAdapter;
-import co.ke.biofit.haemotyping.fragment.BrowseFragment;
-import co.ke.biofit.haemotyping.fragment.HomeFragment;
-import co.ke.biofit.haemotyping.util.MediaUtils;
-import co.ke.biofit.haemotyping.util.TabUtils;
 
-
-@SuppressWarnings("ALL")
-public class MainActivity extends AppCompatActivity implements ActionBar.TabListener {
-    private static final androidx.fragment.app.Fragment[] mFragments ={
-            new androidx.fragment.app.Fragment(),
-    };
-
-    private ArrayList<ActionBar.Tab> mTabs;
-    private ViewPager mViewPager;
-    private androidx.appcompat.app.ActionBar mActionBar;
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.snapButton) Button mSnapButton;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
-        setupActionBar();
+        mSnapButton.setOnClickListener(this);
     }
 
-    private void setupActionBar() {
-        mActionBar = getSupportActionBar();
-        mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        setupPageViewer();
-        mTabs = new ArrayList<ActionBar.Tab>();
-        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            ActionBar.Tab t = mActionBar.newTab().setText(TabUtils.getTabTitleId(i)).setTabListener(this);
-            mTabs.add(t);
-            mActionBar.addTab(t);
+    @Override
+    public void onClick(View view) {
+        if(view == mSnapButton) {
+            Intent intent = new Intent(MainActivity.this, CollectedSampleActivity.class);
+            startActivity(intent);
         }
-    }
-
-
-    @SuppressLint("WrongViewCast")
-    private void setupPageViewer() {
-        mSectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager(), mFragments);
-        mViewPager = (ViewPager) findViewById(R.id.pager);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-            @Override
-            public void onPageSelected(int position) {
-                getSupportActionBar().setSelectedNavigationItem(position);
-            }
-        });
-    }
-
-    // Inflate the menu; this adds items to the action bar if it is present.
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        final MenuItem actionSnapIt = menu.findItem(R.id.action_snap_it);
-        MenuItemCompat.getActionView(actionSnapIt).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onOptionsItemSelected(actionSnapIt);
-            }
-        });
-
-        return super.onCreateOptionsMenu(menu);
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_snap_it:
-                if (MediaUtils.isExternalStorageWritable()) {
-                    Intent intent = new Intent(this, CameraActivity.class);
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, "External Storage not available.",
-                            Toast.LENGTH_SHORT).show();
-                }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-
-
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
-        TabUtils.setCurrentTabPosition(tab.getPosition());
-//        mViewPager.setCurrentItem(tab.getPosition());
-
-    }
-
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-
-    }
-
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
 
     }
 }
