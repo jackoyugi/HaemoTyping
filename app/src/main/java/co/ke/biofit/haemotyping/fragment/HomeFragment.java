ae748e8cd93a07ccdb7d9020b39e8212fb62db48
jackoyugi <oyugijack@gmail.com>
2020-08-03 08:21:13 +0300

    added background image

--- app/src/main/java/co/ke/biofit/haemotyping/fragment/HomeFragment.java
+++ app/src/main/java/co/ke/biofit/haemotyping/fragment/HomeFragment.java
@@ -52,27 +52,27 @@ public class HomeFragment extends DialogFragment {
         return rootView;
 
     }
-//    @Override
-//    public Dialog onCreateDialog(Bundle savedInstanceState) {
-//        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-//        builder.setTitle("Dialog via Builder");
-//        builder.setMessage("Would you like to take a survey?");
-//
-//        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dismiss();
-//            }
-//        });
-//
-//        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
-//            @Override
-//            public void onClick(DialogInterface dialog, int which) {
-//                dismiss();
-//            }
-//        });
-//        return builder.create();
-//    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Dialog via Builder");
+        builder.setMessage("Would you like to take a survey?");
+
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+
+        builder.setNegativeButton("Nope", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dismiss();
+            }
+        });
+        return builder.create();
+    }
 
 }
 
