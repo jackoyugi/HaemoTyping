39fb24a6836558a38a124769650039b8cdc1fdee
jackoyugi <oyugijack@gmail.com>
2020-08-03 18:28:02 +0300

    final commits

--- app/src/main/java/co/ke/biofit/haemotyping/adapter/HaemotypeArrayAdapter.java
+++ app/src/main/java/co/ke/biofit/haemotyping/adapter/HaemotypeArrayAdapter.java
@@ -3,13 +3,16 @@ package co.ke.biofit.haemotyping.adapter;
 import android.content.Context;
 import android.graphics.Typeface;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+
+import butterknife.BindView;
+import co.ke.biofit.haemotyping.R;
 
 public class HaemotypeArrayAdapter extends ArrayAdapter {
 
     private Context mContext;
     private String[] mCollectedSample;
     private String[] mBloodGroup;
-
 
     public HaemotypeArrayAdapter(Context mContext, int resource, String[] mCollectedSample, String[] mBloodGroup) {
         super(mContext, resource);
