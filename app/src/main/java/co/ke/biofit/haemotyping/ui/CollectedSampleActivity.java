fc9f5418b23ed768d8a45e050706e8d3a522f0dd
jackoyugi <oyugijack@gmail.com>
2020-08-13 07:30:51 +0300

    added changes

--- app/src/main/java/co/ke/biofit/haemotyping/ui/CollectedSampleActivity.java
+++ app/src/main/java/co/ke/biofit/haemotyping/ui/CollectedSampleActivity.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
@@ -24,10 +25,13 @@ import co.ke.biofit.haemotyping.activity.MakeUpSearchResponsee;
 import co.ke.biofit.haemotyping.activity.ProductColor;
 import co.ke.biofit.haemotyping.adapter.HaemotypeArrayAdapter;
 import co.ke.biofit.haemotyping.service.MakeUpApi;
-import co.ke.biofit.haemotyping.service.MakeUpClient;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class CollectedSampleActivity extends AppCompatActivity {
     public static final String TAG = CollectedSampleActivity.class.getSimpleName();
@@ -35,6 +39,7 @@ public class CollectedSampleActivity extends AppCompatActivity {
     @BindView(R.id.listView) ListView mListView;
     @BindView(R.id.errorTextView) TextView mErrorTextView;
     @BindView(R.id.progressBar) ProgressBar mProgressBar;
+    ArrayList<MakeUpSearchResponse> makeUpSearchResponse=new ArrayList<>();
 
 
     @Override
@@ -42,6 +47,8 @@ public class CollectedSampleActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collectedsample);
         ButterKnife.bind(this);
+
+        getClientResponse();
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -57,48 +64,56 @@ public class CollectedSampleActivity extends AppCompatActivity {
         String brand = intent.getStringExtra("location");
         mLocationTextView.setText("Here are all the people with blood group (?) near you: " + brand);
 
+//        MakeUpApi client = getClient();
 
+    }
 
-        MakeUpApi client = MakeUpClient.getClient();
+    private void getClientResponse() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://makeup-api.herokuapp.com/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        MakeUpApi client=retrofit.create(MakeUpApi.class);
         Call <List<MakeUpSearchResponse>> call = client.getProducts();
 
         call.enqueue(new Callback<List<MakeUpSearchResponse>>() {
             @Override
             public void onResponse(Call<List<MakeUpSearchResponse>> call, Response<List<MakeUpSearchResponse>> response) {
+                makeUpSearchResponse= new ArrayList<>(response.body());
                 Toast.makeText(CollectedSampleActivity.this, "Success", Toast.LENGTH_SHORT).show();
 
             }
 
-
             @Override
             public void onFailure(Call<List<MakeUpSearchResponse>> call, Throwable t) {
                 Toast.makeText(CollectedSampleActivity.this, "Failed", Toast.LENGTH_SHORT).show();
-                hideProgressBar();
-                showFailureMessage();
-                Log.e(TAG, "onFailure: ", t);
+//                hideProgressBar();
+//                showFailureMessage();
+//                Log.e(TAG, "onFailure: ", t);
             }
         });
 
     }
-    private void showFailureMessage() {
-        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
-        mErrorTextView.setVisibility(View.VISIBLE);
-    }
 
-    private void showUnsuccessfulMessage() {
-        mErrorTextView.setText("Something went wrong. Please try again later");
-        mErrorTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void showProducts() {
-        mListView.setVisibility(View.VISIBLE);
-        mLocationTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void hideProgressBar() {
-        mProgressBar.setVisibility(View.GONE);
-    }
+//    private void showFailureMessage() {
+//        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showUnsuccessfulMessage() {
+//        mErrorTextView.setText("Something went wrong. Please try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showProducts() {
+//        mListView.setVisibility(View.VISIBLE);
+//        mLocationTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void hideProgressBar() {
+//        mProgressBar.setVisibility(View.GONE);
+//    }
 
 
 }
