a526d49e8903295e6fb6f5de4ada4f825d169565
jackoyugi <oyugijack@gmail.com>
2020-08-17 09:33:35 +0300

    added new changes

--- app/src/main/java/co/ke/biofit/haemotyping/ui/MakeUpListActivity.java
+++ app/src/main/java/co/ke/biofit/haemotyping/ui/MakeUpListActivity.java
@@ -41,7 +41,7 @@ public class MakeUpListActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_collectedsample);
+        setContentView(R.layout.activity_makeup);
         ButterKnife.bind(this);
 
 
